2a7fd8c2ddf3f9a7a567dca7b281dd91b566c9d6
MK KNIGHT <mk.knight970@gmail.com>
2020-12-16 08:31:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function setup() {
+	createCanvas(500, 400);
+}
+
+var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+function draw() {
+	background(225);
+	
+	fill(0);
+	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
+	
+	fill(0, 255, 0);
+	rect(xpos, ypos, 50, 50);
+	
+	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
+	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+}
+
+function keyPressed() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = -2;
+			break;
+		case 39:
+		case 68:
+			xspeed = 2;
+			break;
+		case 38:
+		case 87:
+			yspeed = -2;
+			break;
+		case 40:
+		case 83:
+			yspeed = 2;
+			break;
+	}
+}
+
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = 0;
+			break;
+		case 39:
+		case 68:
+			xspeed = 0;
+			break;
+		case 38:
+		case 87:
+			yspeed = 0;
+			break;
+		case 40:
+		case 83:
+			yspeed = 0;
+			break;
+	}
+}
